fe004ddbc4f3903a0b76e33010e180126ac8cc16
Franco Sanchez Alejandro <francosanche@gmail.com>
2025-03-28 19:06:29 -0300

    re factor main container

--- src/editor/editorComponents/mainContainer.js
+++ src/editor/editorComponents/mainContainer.js
@@ -1,27 +1,22 @@
-import { useState, useContext, useEffect } from "react"
-import { TaskContext, FunctionalityContext } from "./sharedComponents"
-import SideBar from "./sideBar"
-import TaskViewer from "./taskViewer"
-import { useMainContainerLogic } from "../../hooks/useMainContainerLogic"
+import SideBar from "./sideBar";
+import TaskViewer from "./taskViewer";
+import { useMainContainerLogic } from "../../hooks/useMainContainerLogic";
 
 
 const MainContainer = () => {
     // main container
-
-    const {sideBarState, taskViewerState, taskToShow} = useMainContainerLogic()
+    const { sideBarState, taskViewerState, taskToShow } = useMainContainerLogic();
 
     return (
-        <div className="flex flex-row w-full h-screen ">
+        <div className="flex flex-row w-full h-screen">
             <SideBar state={sideBarState}></SideBar>
             <TaskViewer
                 state={taskViewerState}
                 task={taskToShow}>
             </TaskViewer>
         </div>
-    )
+    );
+};
 
 
-}
-
-
-export default MainContainer
+export default MainContainer;
